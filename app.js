753bee93964d7e280e199ceda62ea04190dff331
IvanDrebot <drebot99@gmail.com>
2019-12-09 22:59:25 +0200

    init

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('test')
+let express = require('express');
+let cors = require('cors');
+let mongoose = require('mongoose');
+let app = express();
+let mainRouter = require('./router/main');
+mongoose.connect('mongodb://localhost:27017/moments', ({useNewUrlParser: true}));
+
+app.use(cors({origin: '*'}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('uploads'));
+
+app.use('/api', mainRouter);
+
+app.listen(3000, () => {
+    console.log('listening...')
+});
